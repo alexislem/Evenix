567a6c6bb4d218d0b4263ff3f478f9ae54b3d564
Benjamin Maire <benjamin.maire119@gmail.com>
2025-05-09 16:10:27 +0200

    Ajout du projet SpringBoot vierge

---
+++ Backend/src/main/java/com/evenix/EvenixApplication.java
@@ -0,0 +1,13 @@
+package com.evenix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvenixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvenixApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/evenix/EvenixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvenixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
