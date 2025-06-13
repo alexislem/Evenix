9e8e565d72e3664a215f0918691e9b3c0568a220
LEMAIRE Alexis <alexis.lemaire0606@gmail.Com>
2025-06-13 16:42:16 +0200

    ajout d'un jsonignore pour éviter une boucle dans postman

--- Backend/src/main/java/com/evenix/entities/Role.java
+++ Backend/src/main/java/com/evenix/entities/Role.java
@@ -3,6 +3,8 @@ package com.evenix.entities;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -24,6 +26,7 @@ public class Role {
 	private String nom;
 	
 	@OneToMany(mappedBy ="role")
+	@JsonIgnore
 	private List<Utilisateur> utilisateurs = new ArrayList<>();
 	
 	// Constructors
