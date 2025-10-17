d905bc9776f41b9cee66315760d0fbe19585df1d
Benjamin Maire <benjamin.maire119@gmail.com>
2025-10-17 11:09:32 +0200

    Update SecParams.java

---
+++ Backend/src/main/java/com/evenix/security/SecParams.java
@@ -0,0 +1,9 @@
+package com.evenix.security;
+
+public interface SecParams {
+public static final long EXP_TIME = 10*24*60*60;
+public static final String SECRET ="exempleb@yahoo.com"; 
+public static final String PREFIX= "Bearer ";
+public static final String HEADER = "Authorization";
+}
+
